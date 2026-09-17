a8a2d6dbd0522f4d0558fcde457b55bd7961a496
Isha Priyadarshini G <ishapriyadarshini.g@gmail.com>
2026-09-17 14:30:10 +0530

    Time: 14 ms (91.83%), Space: 43.4 MB (89.51%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -4,11 +4,11 @@ class Solution {
         int start=0;
         int end=0;
         int len=0;
-        for(int i=0;i<n;i++){
+         for(int i=0;i<n;i++){
             int len1=expand(s,i,i);
             int len2=expand(s,i,i+1);
             len=Math.max(len1,len2);
-            if(len > end-start){
+            if(len > end-start+1){
                 start=i-(len-1)/2;
                 end=i+(len/2);
             }
